d4c685f6e08f485daa63ecf662eb9dff2f6bcac7
ZT <1842379739@qq.com>
2023-08-20 11:41:54 +0800

    createComponents

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="main">
+    <Header title="喵喵影院"></Header>
+    <div id="content">
+      <div class="cinema_menu">
+        <div class="city_switch">
+          全城 <i class="iconfont icon-lower-triangle"></i>
+        </div>
+        <div class="brand_swtich">
+          品牌 <i class="iconfont icon-lower-triangle"></i>
+        </div>
+        <div class="feature_switch">
+          特色 <i class="iconfont icon-lower-triangle"></i>
+        </div>
+      </div>
+      <!-- 添加组件 -->
+      <CiList></CiList>
+    </div>
+    <Tabbar></Tabbar>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import Tabbar from "@/components/Tabbar";
+import CiList from "@/components/CiList";
+export default {
+  // 加上组件的name属性，在浏览器的vue插件中，调试时，会出现你自定义的组件名，方便调试使用
+  name: "Cinema",
+  components: {
+    Header,
+    Tabbar,
+    CiList
+  },
+};
+</script>
+
+<style scoped>
+#content .cinema_menu {
+  width: 100%;
+  height: 45px;
+  border-bottom: 1px solid #e6e6e6;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background: white;
+}
+
+</style>
